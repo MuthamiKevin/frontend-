2f501735ccf51cdd590a462ade5632e9161e984b
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-19 23:17:25 +0300

    update

--- petshop/shop.js
+++ petshop/shop.js
@@ -34,3 +34,17 @@ const srLeft = ScrollReveal({
     })
     srLeft.reveal('.about-info',{delay: 100})
     srLeft.reveal('.contact-info',{delay: 100})
+    const editor = grapesjs.init({
+        // Indicate where to init the editor. You can also pass an HTMLElement
+        container: '#gjs',
+        // Get the content for the canvas directly from the element
+        // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+        fromElement: true,
+        // Size of the editor
+        height: '300px',
+        width: 'auto',
+        // Disable the storage manager for the moment
+        storageManager: false,
+        // Avoid any default panel
+        panels: { defaults: [] },
+      });
